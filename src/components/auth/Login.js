df5eda8be46ac2ee50fd9760325e6d1ad7f90f21
trey shanks <shanksleslietrey@gmail.com>
2021-12-17 11:16:28 -0600

    fixed

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -43,7 +43,7 @@ export const Login = () => {
             </dialog>
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Level Up</h1>
+                    <h1>Librogam</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputUsername"> Username</label>
