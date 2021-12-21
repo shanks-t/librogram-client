88d269b24442827a637c40b730510f0db59946d1
Trey Shanks <73371937+shanks-t@users.noreply.github.com>
2021-12-20 19:53:42 -0600

    Revert "Ts material"

--- src/components/readingGoal/ReadingGoalsList.js
+++ src/components/readingGoal/ReadingGoalsList.js
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react"
 import { Link, useHistory } from 'react-router-dom'
-import { ProgressBar } from './ProgressBar'
-import { getCurrentUser, getGoalsByUser } from "./ReadingGoalManager"
+import { getReadingGoals } from "./ReadingGoalManager"
 import "./ReadingGoal.css"
 
 export const ReadingGoalsList = (props) => {
     const [ goals, setGoals ] = useState([])
-    const [ userId, setUserId ] = useState()
     const history = useHistory()
 
-    const getUserId = () => {
-        getCurrentUser().then(data => setUserId(data.id))
-    }
     const goalsFetch = () => {
-        getGoalsByUser().then(data => setGoals(data))
+        getReadingGoals(). then(data => setGoals(data))
     }
     
     useEffect(() => {
         goalsFetch()
-    }, [userId]);
-    
-    useEffect(() => {
-       getUserId()
     }, []);
 
     useEffect(() => {
@@ -43,10 +34,6 @@ export const ReadingGoalsList = (props) => {
                     {goal.number_of_books ? (<li>number of books:{goal.number_of_books}</li>) : null}
                     <li>start date:{goal.start_date}</li>
                     <li>end date:{goal.end_date}</li>
-                    <div className='progress'>
-                    <ProgressBar completed={goal.status} />
-
-                    </div>
                     {goal.status == 'completed' ? (<li className='complete'><b >You Completed This Reading Goal!</b></li>) : (<li className='incomplete'><b>Reading Goal Incomplete</b></li>)}
                     </>
             })
